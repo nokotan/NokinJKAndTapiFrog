cf1db97410cec9a7e3cb16846b9dceeb272b5e89
Owner <nokotan>
2019-10-11 06:37:37 +0900

    クリアカウント入れてみた"

--- Assets/StageSelect/StageSelectUIControl.cs
+++ Assets/StageSelect/StageSelectUIControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// ステージセレクト画面のユーザーの入力をハンドリングし、各サブコンポーネントに伝令します
@@ -13,14 +14,36 @@ public class StageSelectUIControl : SingletonMonoBehaviour<StageSelectUIControl>
 
     public int SelectedStage => selectedStage;
 
+    [SerializeField] Text InfoWindow;
+
+    ClearCountManager clearData;
+
     // Start is called before the first frame update
     void Start()
     {     
         animator = GetComponentInChildren<StageSelectAnimationControl>();
         selectedStageMax = animator.selectedStageMax;
+
+        clearData = ClearCountManager.CreateInstance();
+        clearData.Load();
+
+        UpdateInfoWindow();
     }
 
+    void UpdateInfoWindow()
+    {
+        var stageClearData = clearData.GetStageClearCount(selectedStage);
 
+        if (stageClearData == null || stageClearData.TotalTrialCount == 0)
+        {
+            InfoWindow.text = "Clear Rate: -% (0/0)";
+        }
+        else
+        {
+            var ratio = stageClearData.TotalClearCount / stageClearData.TotalTrialCount * 100.0f;
+            InfoWindow.text = $"Clear Rate: {ratio.ToString("F2")}% ({stageClearData.TotalClearCount}/{stageClearData.TotalTrialCount})";
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,12 +53,14 @@ public class StageSelectUIControl : SingletonMonoBehaviour<StageSelectUIControl>
             if (Input.GetAxisRaw("Horizontal") < -0.9f && selectedStage > 1)
             {
                 animator.SetFocus(selectedStage - 1);
-                selectedStage--;               
+                selectedStage--;
+                UpdateInfoWindow();
             }
             else if (Input.GetAxisRaw("Horizontal") > 0.9f && selectedStage < selectedStageMax)
             {
                 animator.SetFocus(selectedStage + 1);
                 selectedStage++;
+                UpdateInfoWindow();
             }
         }   
     }
